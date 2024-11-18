0ce4e71ad0ed8195a9014b64d0c3efc57407ea7b
SooBin_Lee <i_romay@naver.com>
2024-11-18 18:09:25 +0900

    Feat : 글 리스트 테이블형식으로 불러오기 구현

--- src/pages/board/BoardElement.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BoardElement = (props) => {
-  return <div></div>;
-};
-
-export default BoardElement;
--- src/pages/board/boardPage.js
+++ src/pages/board/boardPage.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
-import Header from "../../components/ui/Header";
-import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import styles from "./BoardPage.module.css";
+import React, { useState, useEffect } from 'react';
+import Header from '../../components/ui/Header';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import styles from './BoardPage.module.css';
 
 const BoardPage = () => {
-  const baseUrl = "http://localhost:8080";
+  const baseUrl = 'http://localhost:8080';
   const navigate = useNavigate();
   const [isAuthorized, setIsAuthorized] = useState(false);
   const [nowPage, setNowPage] = useState(1);
@@ -16,13 +16,13 @@ const BoardPage = () => {
 
   const postButtonHandelr = () => {
     // 간단한 토큰 검증 메서드
-    const token = localStorage.getItem("Authorization");
+    const token = localStorage.getItem('Authorization');
     if (token) {
       // 예: 토큰이 존재하면 통과로 간주
       setIsAuthorized(true);
     } else {
-      alert("로그인이 필요합니다.");
-      navigate("/login");
+      alert('로그인이 필요합니다.');
+      navigate('/login');
     }
   };
 
@@ -30,33 +30,45 @@ const BoardPage = () => {
     // 게시글 목록을 서버에서 불러오는 요청
     setLoading(true);
     axios
-      .get(baseUrl + "/board/content/list?page=" + nowPage) // 실제 API URL을 입력하세요.
+      .get(baseUrl + '/board/content/list?page=' + nowPage) // 실제 API URL을 입력하세요.
       .then((response) => {
         setBoardList(response.data.data); // 게시글 목록을 상태에 저장
       })
       .catch((error) => {
-        alert("에러가 발생하였습니다.");
+        alert('에러가 발생하였습니다.');
       })
       .finally(() => {
         setLoading(false);
       });
-  }, [nowPage]); // isAuthorized 값이 변경될 때마다 실행
+  }, [nowPage]); // 현재 페이지 값이 변할때마다 실행
 
   return (
     <div>
       <Header />
       <div className={styles.container}>
         {loading && <p>게시글을 불러오는 중...</p>} {/* 로딩 중일 때 메시지 */}
-        <ul>
-          {boardList.map((board, index) => (
-            <li key={index}>
-              <h3>{board.boardTitle}</h3>
-              <p>{board.boardContent}</p>
-              <p>작성자: {board.boardAuthor}</p>
-              <p>조회수: {board.boardView}</p>
-            </li>
-          ))}
-        </ul>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th className={styles.titleColumn}>제목</th>
+              <th className={styles.authorColumn}>작성자</th>
+              <th className={styles.dateColumn}>날짜</th>
+              <th className={styles.viewColumn}>조회수</th>
+            </tr>
+          </thead>
+          <tbody>
+            {boardList.map((board, index) => (
+              <tr key={index}>
+                <td className={styles.titleColumn}>{board.boardTitle}</td>
+                <td className={styles.authorColumn}>{board.boardAuthor}</td>
+                <td className={styles.dateColumn}>
+                  {board.boardPostDate.slice(0, 10)}
+                </td>
+                <td className={styles.viewColumn}>{board.boardView}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
         {isAuthorized ? (
           <Link to="/boardPost">
             <Button variant="outline-dark">글쓰기</Button>
